4fe887bf3c898fda2784c7aa9d460d4c88e6607f
NGG-kang <77260277+NGG-kang@users.noreply.github.com>
2021-02-03 20:43:28 +0900

    rest practice

--- TodoList/TodoList/settings.py
+++ TodoList/TodoList/settings.py
@@ -35,12 +35,14 @@ ALLOWED_HOSTS = ['localhost',
 
 INSTALLED_APPS = [
     'toDo.apps.TodoConfig',
+    'resetframework.apps.RestframeworkConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +112,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
---
+++ TodoList/restframework/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns= [
+    # path('api/', include('restframwork.urls')),
+    # path('', views.IndexView.as_view(), name='index'),
+
+]
